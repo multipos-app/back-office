2cdb891371b648614ca8c0dfd28c1dcf9c76c2a9
q <qolson@posappliance.com>
2024-10-30 10:55:57 -0800

    modify to item downloads

--- src/Controller/PosDownloadController.php
+++ src/Controller/PosDownloadController.php
@@ -52,23 +52,7 @@ class PosDownloadController extends PosApiController {
     public function index () {
         
         $this->debug ($this->request->getData ());
-
-        // if ($this->request->getData () ['android_id'] == 'cfccbbb3d52c9d9b') {
-        
-        // $this->debug ($this->request->getData ());
-        // }
-        
-        /* $this->debug (sprintf ('pos download..... %-30s -> %10s %3d %4d %4d %20s %8d %8d %8d %20s',
-			*                        $_SERVER ['SERVER_NAME'],
-			*                        $this->request->getData ()  ['dbname'],
-			*                        $this->request->getData ()  ['business_unit_id'],
-			*                        $this->request->getData ()  ['pos_device_id'],
-			*                        $this->request->getData ()  ['device_data'] ['version_code'],
-			*                        $this->request->getData ()  ['device_data'] ['model'] ,
-			*                        $this->request->getData ()  ['pos_config_id'],
-			*                        $this->request->getData ()  ['update_id'],
-			*                        $this->request->getData ()  ['download_count'],
-			*                        $androidID));*/
+		  $buID = $this->request->getData () ['business_unit_id'];
         
         if (!isset ($this->request->getData ()  ['merchant_id'])) {
             
@@ -78,47 +62,10 @@ class PosDownloadController extends PosApiController {
         }
         
         $this->dbconnect ('m_' . $this->request->getData ()  ['merchant_id']);
-		  
-        /* $posTable = TableRegistry::get ('PosUnits');
-
-			* if ($this->request->getData ()  ['download_count'] == 0) {  // pos restart or token update
-			*     
-			*     $token = '';
-			*     if (isset ($this->request->getData ()  ['token'])) {
-			*         
-			*         $token = $this->request->getData ()  ['token'];
-			*     }
-			*     
-			*     $query = $posTable
-			*            ->find ()
-			*            ->where (['pos_no' => $this->request->getData ()  ['pos_no']]);
-			*     
-			*     $pos = $query->first ();
-			*     
-			*     if ($pos) {
-			*         
-			*         $pos ['business_unit_id'] = $this->request->getData ()  ['business_unit_id'];
-			*         $pos ['android_id'] = $this->request->getData ()  ['android_id'];
-			*         $pos ['token'] =  $token;
-			*         $pos ['restart_time'] = time ();
-
-			*         foreach (['version_name', 'android_release', 'version_code', 'model', 'metrics', 'sdk', 'display_height', 'display_width'] as $param) {
-
-			*             if (isset ($this->request->getData ()  ['device_data'] [$param])) {
-
-			*                 $pos [$param] = $this->request->getData ()  ['device_data'] [$param];
-			*             }
-			*         }
-			*         
-			*         $posTable->save ($pos);
-			*     }
-			* }*/
-        
+		          
         $limit = 100;
 
         $where = ['id > ' . $this->request->getData ()  ['update_id']];
-
-		  // $this->debug ($where);
 		  
         $query = TableRegistry::get ('BatchEntries')
 										->find ()
@@ -134,30 +81,10 @@ class PosDownloadController extends PosApiController {
         $total = $query->count ();
         $updates = [];
 		  
-		  /* $this->debug ("updates... $total " . $this->request->getData ()  ['update_id']);
-			*/
         foreach ($query as $update) {
 
-            $update = $update->toArray ();
-
-				/* $this->debug ($update);*/
-				
+            $update = $update->toArray ();				
 				$where = ['id' => $update ['update_id']];
-				$contain = [];
-				$join = [];
-				
-				switch ($update ['update_table']) {
-
-					 case 'items':
-
-						  $where = ['Items.id' => $update ['update_id']];
-						  $contain = ['ItemPrices', 'ItemLinks'];
-						  $join = ['table' => 'item_prices',
-                             'type' => 'left',
-                             'conditions' => 'Items.id = item_prices.item_id'];
-						  break;
-				}
-
 				
 				unset ($update ['update_time']);
 				unset ($update ['execution_time']);
@@ -165,125 +92,146 @@ class PosDownloadController extends PosApiController {
 				switch ($update ['update_action']) {
 						  
 					 case 0:
-						  
-						  $query = TableRegistry::get ($this->models [$update ['update_table']])
-														->find ()
-														->where ($where)
-														->contain ($contain)
-														->join ($join);
-						  
-						  /* $this->debug ($query);*/
-						  
-						  if ($entity = $query->first ()) {
 
-								/* $this->debug ($entity);
-								 */
-								$entity = $entity->toArray ();
-                        
-								unset ($entity ['update_time']);
-								
-								switch ($update ['update_table']) {
-										  
-									 case 'departments':
-										  
-										  unset ($entity ['pricing']);
-										  $entity ['tax_group_id'] = 0;
-										  $entity ['update_time'] = "'".date ('Y-m-d H:i:s')."'";
-										  break;
+						  switch ($update ['update_table']) {
 
-									 case 'items':
-										  
-										  unset ($entity ['supplier_id']);
-										  break;
+								case 'items':
+									 
+									 $item = TableRegistry::get ('Items')
+																 ->find ()
+																 ->where (['id' => $update ['update_id']])
+																 ->first ();
+									 
+									 if ($item) {
 
-									 case 'item_links':
-										  break;
+										  $item = $item->toArray ();
 
-									 case 'addons':
-										  
-										  $entity ['description'] = $entity ['print_description'];
-										  unset ($entity ['print_description']);
-										  break;
-										  
-									 case 'addon_links':
-										  
-										  $entity ['addon_id'] = $entity ['addon_id'];
-										  unset ($entity ['addon_id']);
-										  break;
+										  foreach (['item_prices' => 'ItemPrices', 'item_links' => 'ItemLinks'] as $assoc => $table) {
 
-									 case 'employees':
-										  
-										  unset ($entity ['logged_on']);
-										  unset ($entity ['hourly_rate']);
-										  unset ($entity ['ot_eligible']);            
-										  break;
+												if (!isset ($item [$assoc])) {
 
-									 case 'bo_updates':
-										  
-										  unset ($entity ['execution_time']);
-										  unset ($entity ['batch_id']);
-										  break;
-										  
-									 case 'pos_configs':
-
-										  if ($entity ['id'] != $this->request->getData () ['pos_config_id']) {
+													 $item [$assoc] = [];
+												}
 												
-												$update = null;
-												$total --;
-												// $this->debug ('drop config... ' . $entity ['id']);
-										  }
-										  else {
+												$query = TableRegistry::get ($table)
+																			 ->find ()
+																			 ->where (['item_id' => $item ['id'],
+																						  'business_unit_id' => $buID]);												
+												foreach ($query as $t) {
 
-												$tmp = json_decode ($entity ['config'], true);
-												
-												unset ($tmp ['edit_menus']);
-												$entity ['config'] = json_encode ($tmp);
-												
-												unset ($entity ['config_desc']);
-												unset ($entity ['create_time']);
-												unset ($entity ['config_type']);
-												unset ($entity ['status']);
-												unset ($entity ['business_unit_id']);
-												unset ($entity ['config_template']);
+													 $item [$assoc] [] = $t->toArray ();
+												}
 										  }
 										  
-										  break;
-										  
-									 case 'addons':
-									 case 'addon_links':
-
-										  break;
-										  
-									 case 'profiles':
-										  
-										  $update ['update'] = $entity;
-										  
+										  $update ['update'] = $item;
 										  $updates [] = $update;
 										  
-										  $permissions = TableRegistry::get ('ProfilePermissions')
-																				->find ()
-																				->where (['profile_id' => $update ['update'] ['id']]);
-										  
-										  foreach ($permissions as $permission) {
-												
-												$update ['update'] = $permission->toArray ();
-												$update ['update_table'] = 'profile_permissions';
-												$updates [] = $update;
-										  }
-
-										  $update = null;
-										  break;
-								}
-
-								if ($update != null) {
+										  unset ($item ['supplier_id']);
+									 }
 									 
-									 $update ['update'] = $entity;
-									 $updates [] = $update;
-								}
-						  }
-						  else {
-								
-								$this->debug ('update not found...' . $update ['update_id']);   
+									 break;
+									 
+								default:
+									 
+									 $query = TableRegistry::get ($this->models [$update ['update_table']])
+																  ->find ()
+																  ->where ($where);
+									 									 
+									 if ($entity = $query->first ()) {
+										  
+										  $entity = $entity->toArray ();
+										  
+										  unset ($entity ['update_time']);
+										  
+										  switch ($update ['update_table']) {
+													 
+												case 'departments':
+													 
+													 unset ($entity ['pricing']);
+													 $entity ['tax_group_id'] = 0;
+													 $entity ['update_time'] = "'".date ('Y-m-d H:i:s')."'";
+													 break;
+
+												case 'addons':
+													 
+													 $entity ['description'] = $entity ['print_description'];
+													 unset ($entity ['print_description']);
+													 break;
+													 
+												case 'addon_links':
+													 
+													 $entity ['addon_id'] = $entity ['addon_id'];
+													 unset ($entity ['addon_id']);
+													 break;
+
+												case 'employees':
+													 
+													 unset ($entity ['logged_on']);
+													 unset ($entity ['hourly_rate']);
+													 unset ($entity ['ot_eligible']);            
+													 break;
+
+												case 'bo_updates':
+													 
+													 unset ($entity ['execution_time']);
+													 unset ($entity ['batch_id']);
+													 break;
+													 
+												case 'pos_configs':
+
+													 if ($entity ['id'] != $this->request->getData () ['pos_config_id']) {
+														  
+														  $update = null;
+														  $total --;
+													 }
+													 else {
+
+														  $tmp = json_decode ($entity ['config'], true);
+														  
+														  unset ($tmp ['edit_menus']);
+														  $entity ['config'] = json_encode ($tmp);
+														  
+														  unset ($entity ['create_time']);
+														  unset ($entity ['config_type']);
+														  unset ($entity ['status']);
+														  unset ($entity ['business_unit_id']);
+														  unset ($entity ['config_template']);
+													 }
+													 
+													 break;
+													 
+												case 'addons':
+												case 'addon_links':
+
+													 break;
+													 
+												case 'profiles':
+													 
+													 $update ['update'] = $entity;
+													 
+													 $updates [] = $update;
+													 
+													 $permissions = TableRegistry::get ('ProfilePermissions')
+																						  ->find ()
+																						  ->where (['profile_id' => $update ['update'] ['id']]);
+													 
+													 foreach ($permissions as $permission) {
+														  
+														  $update ['update'] = $permission->toArray ();
+														  $update ['update_table'] = 'profile_permissions';
+														  $updates [] = $update;
+													 }
+
+													 $update = null;
+													 break;
+										  }
+									 }
+
+									 if ($update != null) {
+										  
+										  $update ['update'] = $entity;
+										  $updates [] = $update;
+									 }
 						  }
 						  
 						  break;
@@ -300,8 +248,6 @@ class PosDownloadController extends PosApiController {
 						  break;
 				}  
 		  }
-
-		  // $this->debug ($updates);
 		  
 		  $response = ['count' => count ($updates),
 							'total' => $total,
